e3274dcaba7dda148d38fd103b82208dcc02d066
Sandro Santilli <strk@kbt.io>
2017-08-21 15:29:28 +0200

    Drop unused $Id$ tags

--- scripts/make_charmap.pl
+++ scripts/make_charmap.pl
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# $Id$
+#
 #
 # Copyright 2011 Crown copyright (c)
 # Land Information New Zealand and the New Zealand Government.
